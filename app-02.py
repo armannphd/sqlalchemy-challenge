8e78f0c9ebd317ac442d5c7474499a257c3d2ed6
Armann <115322974+armannphd@users.noreply.github.com>
2023-03-01 09:02:32 -0800

    final-02

--- app-02.py
+++ app-02.py
@@ -41,8 +41,7 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/date"
+        f"/api/v1.0/tobs"
     )
 
 
